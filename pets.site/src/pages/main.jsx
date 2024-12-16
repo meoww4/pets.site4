fba4d673068d85fcc7db44d817cc5d7f0dc541dc
meoww4 <mashafetrovav@gmail.com>
2024-12-16 18:58:35 +0300

    22

--- pets.site/src/pages/main.jsx
+++ pets.site/src/pages/main.jsx
@@ -1,22 +1,46 @@
-import React from 'react';
-import One from '../components/One';
-import Two from '../components/Two';
-import Three from '../components/Three';
-import Cart1 from '../components/cart1';
-import Cart2 from '../components/cart2';
-import Slider from '../components/slider';
-
+import React, { useEffect, useState } from "react";
+import Slider from "../components/slider";
+import Card from "../components/card";
+import Two from "../components/Two";
+import Three from "../components/Three";
 
 const Main = () => {
-    return (
-        <div>
-            <Slider/>
-            <Two/>
-            <Cart1/>
-            <Cart2/>
-            <Three/>
+  let [pet, SetPet] = useState([]);
+
+  useEffect(() => {
+    load();
+  }, []);
+
+  function load() {
+    fetch("https://pets.сделай.site/api/pets")
+      .then((response) => response.json())
+      .then((result) => {
+        console.log(result);
+        SetPet(result.data.orders);
+      });
+  }
+
+  return (
+    <div>
+      <Slider />
+      <div>
+        <h2 className="text-center text-white bg-primary m-2">
+          Карточки найденных животных
+        </h2>
+      </div>
+      <div className="container mt-4">
+        <div className="row">
+          {pet.map((item) => (
+            <div key={item.id} className="col-md-6 col-lg-4 mb-4">
+              <Card data={item} />
+            </div>
+          ))}
         </div>
-    );
+      </div>
+      <Two />
+      <Three />
+    </div>
+  );
 };
 
 export default Main;
